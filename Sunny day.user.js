0d7fcd793cd2fe3946c908f2a7c8b3b51c124539
tukkek <tukkek@gmail.com>
2021-10-22 19:19:50 -0300

    BLACKLIST and WHITELIST tags

--- Sunny day.user.js
+++ Sunny day.user.js
@@ -8,22 +8,29 @@
 // @icon         https://www.google.com/s2/favicons?domain=twitch.tv
 // @grant        none
 // ==/UserScript==
+
+//sadly these need to be case-perfect and escaped with CSS syntax if there's any punctuation or symbols in them
+const BLACKLIST=[] //remove channels with these tags
+const WHITELIST=[] //remove channels without these tags
+
 const REFRESH=3 //run filter every few seconds, raise if this slows down your browser
 const MAXTITLE=30 //maximum allowed stream title character count
-const TAGS=[] //sadly this needs to be case-perfect
 
-function remove(channel){
-    let title=channel.querySelector('*[data-a-target=preview-card-title-link]')
-    if(!title) return false
-    if(title.textContent.length>=MAXTITLE) return true
-    for(let t of TAGS) if(channel.querySelector(`*[data-a-target=${t}]`))
+function find(tags,channel){
+    for(let t of tags) if(channel.querySelector(`*[data-a-target=${t}]`))
         return true
     return false
 }
 
+function allow(channel){
+    let title=channel.querySelector('*[data-a-target=preview-card-title-link]')
+    return title&&title.textContent.length>=MAXTITLE&&
+        find(WHITELIST,channel)&&!find(BLACKLIST,channel)
+}
+
 function clear(){
     for(let c of document.querySelectorAll('*[data-target=directory-container]>*>*'))
-        if(remove(c)) c.remove()
+        if(!allow(c)) c.remove()
 }
 
 setInterval(clear,REFRESH*1000);
