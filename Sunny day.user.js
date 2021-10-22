9fc81de59947ac9732be58c03f1122f93322a7db
tukkek <tukkek@gmail.com>
2021-10-22 20:14:51 -0300

    CHECKCASE

--- Sunny day.user.js
+++ Sunny day.user.js
@@ -9,9 +9,13 @@
 // @grant        none
 // ==/UserScript==
 const BLACKLIST=[] //remove channels with these tags
-const WHITELIST=[] //remove channels without these tags
-const MAXTITLE=30 //maximum allowed stream title character count
-const REFRESH=3 //run filter every few seconds, use a higher period if this script slows down your browser
+const WHITELIST=[] //remove channels without at least onf of these tags
+const MAXTITLE=30 //maximum allowed stream title length
+const CHECKCASE=true //check for a first-letter uppercase and at least one lowercase letter in title
+const REFRESH=3 // run filter every few seconds, use a higher number if this script slows Twitch down
+
+const UPPERCASE=new RegExp('[A-Z]')
+const LOWERCASE=new RegExp('[a-z]')
 
 function find(tags,channel){
     for(let t of tags) if(channel.querySelector(`*[data-a-target="${CSS.escape(t)}" i]`))
@@ -19,10 +23,20 @@ function find(tags,channel){
     return false
 }
 
-function allow(channel){
-    let title=channel.querySelector('*[data-a-target=preview-card-title-link]')
+function check(title){
     if(!title) return true
-    return title.textContent.length<MAXTITLE&&
+    title=title.textContent
+    if(title.length>=MAXTITLE) return false
+    if(CHECKCASE){
+        let cases=Array.from(title)
+        if(!UPPERCASE.exec(cases[0])) return false
+        if(!cases.find(c=>LOWERCASE.exec(c))) return false
+    }
+    return true
+}
+
+function allow(channel){
+    return check(channel.querySelector('*[data-a-target=preview-card-title-link]'))&&
         find(WHITELIST,channel)&&!find(BLACKLIST,channel)
 }
 
